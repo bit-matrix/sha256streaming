ac2b228447bbffe11a877d87cc0a0293d91e3919
brqgoo <burak@bitmatrix.app>
2022-08-17 15:53:27 +0300

    256update

--- sha256streaming/main.cpp
+++ sha256streaming/main.cpp
@@ -10,7 +10,48 @@
 #include <sstream>
 #include "Bytestring.hpp"
 
-std::string sha256context(std::string in) {
+
+std::string sha256update(std::string in1, std::string in2) {
+    
+    ByteString bs(in1);
+    std::string s1 = bs.fromHexString();
+    
+    std::vector<unsigned char> t1;
+    t1.insert(t1.begin(), s1.begin(), s1.end());
+    
+
+   // const unsigned char* t1 = reinterpret_cast<const unsigned char*>(s1.c_str());
+    
+    ByteString bs2(in2);
+    std::string s2 = bs2.fromHexString();
+    const unsigned char* t2 = reinterpret_cast<const unsigned char*>(s2.c_str());
+    
+    CSHA256 cs;
+    
+    cs.Load(t1);
+    cs.SafeWrite(t2,s2.size());
+    
+    std::vector<unsigned char> l;
+    l = cs.Save();
+
+    std::string string;
+
+    char const hex_chars[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+    for( int i = 0; i < l.size(); ++i )
+    {
+        char const byte = l[i];
+
+        string += hex_chars[ ( byte & 0xF0 ) >> 4 ];
+        string += hex_chars[ ( byte & 0x0F ) >> 0 ];
+    }
+
+    return string;
+    
+}
+
+
+std::string sha256initialize(std::string in) {
     
     ByteString bs(in);
     std::string s = bs.fromHexString();
@@ -39,10 +80,56 @@ std::string sha256context(std::string in) {
 }
 
 int main() {
-    std::cout << "Enter data for SHA256 context > " << std::flush;
-    std::string input;
-    std::cin >> input;
-    std::cout << "SHA256 context is:" << std::endl;
-    std::cout << sha256context(input) << std::endl;
+    std::cout << "1.\t" << "OP_SHA256INITIALIZE" << std::endl;
+    std::cout << "2.\t" << "OP_SHA256UPDATE" << std::endl;
+    std::cout << "3.\t" << "OP_SHA256FINALIZE" << std::endl;
+    std::cout << "Option selection > " << std::flush;
+    int selection;
+    std::cin >> selection;
+    
+    switch (selection) {
+        case 1:
+        {
+            std::cout << "Enter data for SHA256 context > " << std::flush;
+            std::string dataForContext;
+            std::cin >> dataForContext;
+            std::cout << "SHA256 context is:" << std::endl;
+            std::cout << sha256initialize(dataForContext) << std::endl;
+            break;
+        }
+
+        case 2:
+        {
+            std::cout << "Enter previous SHA256 context > " << std::flush;
+            std::string prev256context;
+            std::cin >> prev256context;
+            
+            std::cout << "Enter new data for SHA256 context > " << std::flush;
+            std::string newDataForContext;
+            std::cin >> newDataForContext;
+            
+            std::cout << "Updated SHA256 context is:" << std::endl;
+            std::cout << sha256update(prev256context, newDataForContext) << std::endl;
+            
+            break;
+        }
+            
+        case 3:
+        {
+            std::cout << "case2" << std::endl;
+            break;
+        }
+            
+        default:
+            break;
+    }
+    
+    
+    
+   // std::cout << "Enter data for SHA256 context > " << std::flush;
+    // std::string input;
+    //std::cin >> input;
+    //std::cout << "SHA256 context is:" << std::endl;
+    //std::cout << sha256context(input) << std::endl;
     return 0;
 }
